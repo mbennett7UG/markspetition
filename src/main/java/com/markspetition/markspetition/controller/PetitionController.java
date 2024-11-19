c169866c256b70091e3a355c80fc65e43608a79f
Marky <m.bennett7@universityofgalway.ie>
2024-11-19 12:54:23 +0000

    Feature: Create New Petitions

--- src/main/java/com/markspetition/markspetition/controller/PetitionController.java
+++ src/main/java/com/markspetition/markspetition/controller/PetitionController.java
@@ -1,11 +1,14 @@
 package com.markspetition.markspetition.controller;
 
+import com.markspetition.markspetition.model.Petition;
 import com.markspetition.markspetition.service.PetitionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/petitions")
@@ -24,6 +27,18 @@ public class PetitionController {
     public String viewAllPetitions(Model model) {
         model.addAttribute("petitions", petitionService.getAllPetitions());
         return "all-petitions"; // This is the page for all petitions
+    }
+
+    @GetMapping("/create")
+    public String createPetitionPage() {
+        return "create-petition";
+    }
+
+    @PostMapping("/create")
+    public String createPetition(@RequestParam String title, @RequestParam String description) {
+        int newId = petitionService.getAllPetitions().size() + 1;
+        petitionService.addPetition(new Petition(newId, title, description));
+        return "redirect:/petitions/home";
     }
 
 
