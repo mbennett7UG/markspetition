da2aa31a2253d03411d0b48e831d64dd65706445
Marky <m.bennett7@universityofgalway.ie>
2024-11-19 11:35:16 +0000

    Initial commit

---
+++ src/main/java/com/markspetition/markspetition/MarkspetitionApplication.java
@@ -0,0 +1,13 @@
+package com.markspetition.markspetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkspetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkspetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markspetition/markspetition/MarkspetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markspetition.markspetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkspetitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
